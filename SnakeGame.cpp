f363128678e369b4fe92bc35b83fb12d766c4474
Ibrahim Khan <101021647+IbrahimKhanGH@users.noreply.github.com>
2022-10-04 17:54:52 -0500

    Update SnakeGame.cpp

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,146 +1,179 @@
-#include <conio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-  
-int i, j, height = 20, width = 20;
-int gameover, score;
-int x, y, fruitx, fruity, flag;
-  
-// Function to generate the fruit
-// within the boundary
-void setup()
+// Ibrahim Khan 
+// Snake Game
+
+
+#include <iostream> // cin, cout
+#include <conio.h>  // console IO
+#include <windows.h>
+
+using namespace std;
+
+// global variables
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirecton dir;
+
+void Setup() // sets up the game and the head of the snake
 {
-    gameover = 0;
-  
-    // Stores height and width
-    x = height / 2;
-    y = width / 2;
-label1:
-    fruitx = rand() % 20;
-    if (fruitx == 0)
-        goto label1;
-label2:
-    fruity = rand() % 20;
-    if (fruity == 0)
-        goto label2;
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2; // position of the snake starts in the middle
+    fruitX = rand() % width;
+    fruitY = rand() % height; // randomizes the position of the fruit
     score = 0;
 }
-  
-// Function to draw the boundaries
-void draw()
+
+void Draw() // this draws the entire game, including border
 {
-    system("cls");
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            if (i == 0 || i == width - 1
-                || j == 0
-                || j == height - 1) {
-                printf("#");
+    system("cls"); //system("clear");
+    for (int i = 0; i < width+2; i++)
+    {
+        cout << "#"; // this makes the border
+    }
+    cout << endl;
+ 
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+            {
+                cout << "#"; // this makes the border
             }
-            else {
-                if (i == x && j == y)
-                    printf("0");
-                else if (i == fruitx
-                         && j == fruity)
-                    printf("*");
-                else
-                    printf(" ");
+            if (i == y && j == x)
+            {
+                cout << "O"; // initial start of snake head
+            }
+            else if (i == fruitY && j == fruitX)
+            {
+                cout << "F"; // adds an F where the fruit is placed
+            }
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o"; // adds to the tail of the snake
+                        print = true;
+                    }
+                }
+                if (!print)
+                {
+                    cout << " ";
+                }            
+            }
+            if (j == width - 1)
+            {
+                cout << "#"; // this prints the border
             }
         }
-        printf("\n");
+        cout << endl;
     }
-  
-    // Print the score after the
-    // game ends
-    printf("score = %d", score);
-    printf("\n");
-    printf("press X to quit the game");
+ 
+    for (int i = 0; i < width+2; i++)
+    {
+        cout << "#"; // this prints the border
+    }
+
+    cout << endl;
+    cout << "Score:" << score << endl; // prints the score
 }
-  
-// Function to take the input
-void input()
+
+void Input() // input of the user
 {
-    if (kbhit()) {
-        switch (getch()) {
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
         case 'a':
-            flag = 1;
-            break;
-        case 's':
-            flag = 2;
+            dir = LEFT;
             break;
         case 'd':
-            flag = 3;
+            dir = RIGHT;
             break;
         case 'w':
-            flag = 4;
+            dir = UP;
             break;
-        case 'x':
-            gameover = 1;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x': // killswicth/endgame
+            gameOver = true;
             break;
         }
     }
 }
-  
-// Function for the logic behind
-// each movement
-void logic()
+
+
+void Logic() // the logic of the game (score addition, snake head running into itself, border reset)
 {
-    sleep(0.01);
-    switch (flag) {
-    case 1:
-        y--;
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir) // the users input changes the position of the snake head
+    {
+    case LEFT:
+        x--;
         break;
-    case 2:
+    case RIGHT:
         x++;
         break;
-    case 3:
-        y++;
+    case UP:
+        y--;
         break;
-    case 4:
-        x--;
+    case DOWN:
+        y++;
         break;
     default:
         break;
     }
   
-    // If the game is over
-    if (x < 0 || x > height
-        || y < 0 || y > width)
-        gameover = 1;
-  
-    // If snake reaches the fruit
-    // then update the score
-    if (x == fruitx && y == fruity) {
-    label3:
-        fruitx = rand() % 20;
-        if (fruitx == 0)
-            goto label3;
-  
-    // After eating the above fruit
-    // generate new fruit
-    label4:
-        fruity = rand() % 20;
-        if (fruity == 0)
-            goto label4;
-        score += 10;
+    if (x > width || x < 0 || y > height || y < 0)
+      gameOver = true;
+ 
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+ 
+    if (x == fruitX && y == fruitY) 
+    {
+        score += 10; // adds 10 to the score and randomizes a new fruit
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++; // adds one to the tail
     }
 }
-  
-// Driver Code
-void main()
+
+int main() // main function
 {
-    int m, n;
-  
-    // Generate boundary
-    setup();
-  
-    // Until the game is over
-    while (!gameover) {
-  
-        // Function Call
-        draw();
-        input();
-        logic();
+    Setup(); // sets up the game
+    while (!gameOver) // keeps running until something ends the game
+    {
+        Draw(); // draws the game
+        Input(); // this is the users movement
+        Logic(); // adds the score and kills the game based on the input
+        Sleep(10); //sleep(10);
     }
+    return 0;
 }
